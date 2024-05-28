fa74d512a986926615206ac145bbf818f138a39b
Arpit Gupta <arpit4gupta4@gmail.com>
2024-05-28 20:00:50 +0530

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqps://b-6d344f1d-1f71-4e0a-8b46-4658ca551769.mq.us-east-1.amazonaws.com:5671"),
+    Port = 5671,
+    UserName = "adminarpit",
+    Password = "Arpitadmin1@"
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "firstQueue",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+channel.BasicConsume(queue: "firstQueue",
+    autoAck: true,
+    consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ Producer/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqps://b-6d344f1d-1f71-4e0a-8b46-4658ca551769.mq.us-east-1.amazonaws.com:5671"),
+    Port = 5671,
+    UserName = "adminarpit",
+    Password = "Arpitadmin1@"
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "firstQueue",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+
+string? message = null;
+do
+{
+    Console.WriteLine("Enter Message. Press [enter] to exit.");
+    message = Console.ReadLine();
+    if (!string.IsNullOrEmpty(message))
+        SendMessage(message, channel);
+} while (!string.IsNullOrEmpty(message));
+
+void SendMessage(string s, IModel channel)
+{
+    var body = Encoding.UTF8.GetBytes(s);
+
+    channel.BasicPublish(exchange: string.Empty,
+        routingKey: "firstQueue",
+        basicProperties: null,
+        body: body);
+    Console.WriteLine($" [x] Sent {s}");
+}
